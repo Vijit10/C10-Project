e765602f97cd175c627ffb0ce6d8af26b9c6580f
Vijit10 <106871787+Vijit10@users.noreply.github.com>
2022-06-11 10:50:27 +0530

    C10 Project

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function preload(){
   shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1");
-  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
   //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
@@ -21,7 +21,7 @@ function setup(){
   sea=createSprite(400,200);
   sea.addImage(seaImg);
   sea.velocityX = -5;
-  sea.scale = 0.3;
+  sea.scale=0.3;
 
   
   ship = createSprite(130,200,30,30);
@@ -34,11 +34,16 @@ function draw() {
   background(0);
   sea.velocityX = -3;
 
+// making ship movement smooth 
+ if(ship.x <= 200){ ship.velocityY = 2 } 
+ if(ship.x > 230){ ship.velocityY = -1 }
+
+
   //uncomment code to reset the background
   if(sea.x < 0){
     //sea.x = 0;
     //sea.x = sea.width;
-    sea.x = sea.width/8;
+    //sea.x = sea.width/8;
     //sea.y = height;
   }
 
